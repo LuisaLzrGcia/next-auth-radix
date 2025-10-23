8212e70737d99877a10bde28dcabe41aafdcbca9
LuisaLzrGcia <maluisa.lzrgcia@gmail.com>
2025-10-23 17:33:49 -0600

    first commit

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,16 @@
+import { BookmarkIcon } from '@radix-ui/react-icons'
+import { Button } from '@radix-ui/themes'
+import React from 'react'
+
+function LoginPage() {
+    return (
+        <>
+            <Button>
+                <BookmarkIcon /> Bookmark
+            </Button>
+
+        </>
+    )
+}
+
+export default LoginPage

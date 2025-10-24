3fa39e1e6e145618b8c081af9fd127e8dafadf3e
LuisaLzrGcia <maluisa.lzrgcia@gmail.com>
2025-10-24 11:45:57 -0600

    Validaciones a formularios de login y registro con react-hook-form

--- src/components/auth/SignupForm.tsx/SignupForm.tsx
+++ src/components/auth/SignupForm.tsx/SignupForm.tsx
@@ -1,35 +1,132 @@
+'use client';
 import { EnvelopeClosedIcon, LockClosedIcon, PersonIcon } from '@radix-ui/react-icons'
-import { Button, Flex, TextField } from '@radix-ui/themes'
+import { Button, Flex, Text, TextField } from '@radix-ui/themes'
 import React from 'react'
+import { Controller, useForm } from 'react-hook-form';
 
 function SignupForm() {
+    const {
+        control,
+        handleSubmit,
+        formState: { errors }
+    } = useForm(
+        {
+            values: {
+                name: '',
+                email: '',
+                password: ''
+            }
+        }
+    );
+
+    const onSubmit = handleSubmit((data) => {
+        console.log(data);
+    })
     return (
         <>
-            <Flex className='h-auto w-full mt-2' direction={"column"} gap={"3"}>
-                <label htmlFor="name">Name:</label>
-                <TextField.Root placeholder="Jhon Doe" autoFocus>
-                    <TextField.Slot>
-                        <PersonIcon />
-                    </TextField.Slot>
-                </TextField.Root>
+            <form onSubmit={onSubmit}>
+                <Flex className='h-auto w-full mt-2' direction={"column"} gap={"3"}>
+                    <label htmlFor="name">Name:</label>
+                    <Controller
+                        name="name"
+                        control={control}
+                        rules={{
+                            required: {
+                                message: "Name is required",
+                                value: true
+                            },
+                            validate: {
+                                noNumbers: (value) =>
+                                    !/\d/.test(value) || "Name cannot contain numbers",
+                            }
+                        }}
+                        render={({ field }) => (
+                            <TextField.Root
+                                placeholder="Jhon Doe"
+                                autoFocus
+                                {...field}>
+                                <TextField.Slot>
+                                    <PersonIcon />
+                                </TextField.Slot>
+                            </TextField.Root>
+                        )}
+                    />
+                    {
+                        errors.name &&
+                        <Text size={'1'} color='ruby'>{errors.name.message}</Text>
+                    }
 
-                <label htmlFor="email">Email:</label>
-                <TextField.Root placeholder="email@domain.com" autoFocus>
-                    <TextField.Slot>
-                        <EnvelopeClosedIcon />
-                    </TextField.Slot>
-                </TextField.Root>
+                    <label htmlFor="email">Email:</label>
+                    <Controller
+                        name="email"
+                        control={control}
+                        rules={{
+                            required: {
+                                message: "Email is required",
+                                value: true
+                            }
+                        }}
+                        render={({ field }) => (
+                            <TextField.Root
+                                type='email'
+                                placeholder="email@domain.com"
+                                autoFocus
+                                {...field}
+                            >
+                                <TextField.Slot>
+                                    <EnvelopeClosedIcon />
+                                </TextField.Slot>
+                            </TextField.Root>
+                        )}
+                    />
+                    {
+                        errors.email &&
+                        <Text size={'1'} color='ruby'>{errors.email.message}</Text>
+                    }
 
-                <label htmlFor="password">Password:</label>
-                <TextField.Root type='password' placeholder="********" autoFocus>
-                    <TextField.Slot>
-                        <LockClosedIcon />
-                    </TextField.Slot>
-                </TextField.Root>
-                <Button>
-                    Sign In
-                </Button>
-            </Flex>
+                    <label htmlFor="password">Password:</label>
+                    <Controller
+                        name="password"
+                        control={control}
+                        rules={{
+                            required: {
+                                message: "Password is required",
+                                value: true
+                            },
+                            minLength: {
+                                message: "Password must be at least 8 characters",
+                                value: 8
+                            },
+                            validate: {
+                                hasNumber: (value) =>
+                                    /[0-9]/.test(value) || "Password must contain at least one number",
+                                hasUpperCase: (value) =>
+                                    /[A-Z]/.test(value) || "Password must contain at least one uppercase letter",
+                                hasLowerCase: (value) =>
+                                    /[a-z]/.test(value) || "Password must contain at least one lowercase letter",
+                            }
+                        }}
+                        render={({ field }) => (
+                            <TextField.Root
+                                type='password'
+                                placeholder="********"
+                                autoFocus
+                                {...field}
+                            >
+                                <TextField.Slot>
+                                    <LockClosedIcon />
+                                </TextField.Slot>
+                            </TextField.Root>
+                        )} />
+                    {
+                        errors.password &&
+                        <Text size={'1'} color='ruby'>{errors.password.message}</Text>
+                    }
+                    <Button type='submit'>
+                        Sign Up
+                    </Button>
+                </Flex>
+            </form>
         </>
     )
 }

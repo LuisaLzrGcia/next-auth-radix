68e97c06cbfe405810f1dee7245ae4280390f6c5
LuisaLzrGcia <maluisa.lzrgcia@gmail.com>
2025-10-29 16:05:29 -0600

    Modificacion de Readme

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,5 @@
 import { authOptions } from "@/libs/authOptions";
-import { prisma } from "@/libs/prisma";
-import bcrypt from "bcryptjs";
-import NextAuth, { AuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-
+import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
 
